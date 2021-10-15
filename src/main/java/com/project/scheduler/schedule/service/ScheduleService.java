42faa4e9e1e64315789ac44d244b5f6c779767c1
yn0315 <yunu0315@naver.com>
2021-10-15 10:51:54 +0900

    10.15 서비스

--- src/main/java/com/project/scheduler/schedule/service/ScheduleService.java
+++ src/main/java/com/project/scheduler/schedule/service/ScheduleService.java
@@ -1,12 +1,40 @@
 package com.project.scheduler.schedule.service;
 
+import com.project.scheduler.schedule.domain.Schedule;
 import com.project.scheduler.schedule.repository.ScheduleMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ScheduleService {
 
     private final ScheduleMapper scheduleMapper;
+
+    // 전체 스케쥴 가져오기
+    List<Schedule> getScheduleList(){
+        return scheduleMapper.getScheduleList();
+    }
+
+    // 스케쥴 세부 일정 가져오기 - 스케쥴 번호를 통해 스케쥴 세부 정보 가져오기
+    Schedule getSchedule(int scdNo){
+     return scheduleMapper.getSchedule(scdNo);
+    }
+
+    // 스케쥴 등록
+    Schedule insertSchedule(Schedule schedule){
+        return scheduleMapper.insertSchedule(schedule);
+    }
+
+    // 스케쥴 삭제
+    boolean deleteSchedule(int scdNo){
+        return scheduleMapper.deleteSchedule(scdNo);
+    }
+
+    // 스케쥴 수정
+    boolean modifySchedule(Schedule schedule){
+        return scheduleMapper.modifySchedule(schedule);
+    }
 }
